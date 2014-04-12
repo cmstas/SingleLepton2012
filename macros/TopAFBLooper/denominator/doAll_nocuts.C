5fde339eaacf0f05b7d621679c01d583f8b7a774
linacre <linacre@fnal.gov>
2014-04-12 12:33:07 -0700

    tidy up

--- macros/TopAFBLooper/denominator/doAll_nocuts.C
+++ macros/TopAFBLooper/denominator/doAll_nocuts.C
@@ -1,32 +1,6 @@
 void doAll_nocuts(TString outputDir = "results")
 {
-    //gSystem->Load("/home/users/yanjuntu/MiniFWlib/libMiniFWLite_v5.28.00.so");
-    // gSystem->Load("/home/users/yanjuntu/MiniFWlib/libMiniFWLite_5.27.06b-cms10.so");
-    gSystem->Load("/home/users/yanjuntu/MiniFWlib/libMiniFWLite_CMSSW_5_3_2_patch4_V05-03-13.so");
-    //gSystem->Load("/nfs-3/userdata/yanjuntu/lhapdf/lib/libLHAPDF.so");
-    //gSystem->Load("/nfs-6/userdata/yanjuntu/LHAPDF/lib/libLHAPDF.so");
-
-    /*
-    gSystem->AddIncludePath(" -w -I../CORE/topmass -I/nfs-6/userdata/yanjuntu/LHAPDF/include");
-    gROOT->ProcessLine(".L ../CORE/topmass/ttdilepsolve.cpp+");
-    gROOT->ProcessLine(".L ../CORE/utilities.cc+");
-    gROOT->ProcessLine(".L ../CORE/trackSelections.cc+");
-    gROOT->ProcessLine(".L ../CORE/eventSelections.cc+");
-    gROOT->ProcessLine(".L ../CORE/MITConversionUtilities.cc+");
-    gROOT->ProcessLine(".L ../CORE/muonSelections.cc+");
-    gROOT->ProcessLine(".L ../CORE/electronSelectionsParameters.cc+");
-    gROOT->ProcessLine(".L ../CORE/electronSelections.cc+");
-    gROOT->ProcessLine(".L ../CORE/metSelections.cc+");
-    gROOT->ProcessLine(".L ../CORE/SimpleFakeRate.cc+");
-    gROOT->ProcessLine(".L ../CORE/MT2/MT2.cc+");
-    gROOT->ProcessLine(".L ../CORE/triggerUtils.cc+");
-    gROOT->ProcessLine(".L ../CORE/susySelections.cc+");
-    //gROOT->ProcessLine(".L ../CORE/mcSUSYkfactor.cc+");
-    gROOT->ProcessLine(".L ../CORE/triggerSuperModel.cc+");
-    gROOT->ProcessLine(".L ../CORE/jetSelections.cc+");
-    gROOT->ProcessLine(".L ../CORE/ttbarSelections.cc+");
-    gROOT->ProcessLine(".L ../CORE/triggerUtils.cc+");
-    */
+    gSystem->Load("../../../Tools/MiniFWLite/libMiniFWLite.so");
 
     gROOT->ProcessLine(".L ../../../CORE/CMS2.cc+");
     gROOT->ProcessLine(".L ../../../CORE/mcSelections.cc+");
@@ -35,17 +9,17 @@ void doAll_nocuts(TString outputDir = "results")
     gSystem->CompileMacro("topAFB_looper.C", "++k", "libtopAFB_looper");
 
 
-    float lumiToNormalizeTo   = 4980.0e-3;
+    float lumiToNormalizeTo   = 19.5;
 
     topAFB_looper *baby = new topAFB_looper();
 
     vector<TString> v_baseCuts;
     v_baseCuts.push_back("applyTopPtWeighting");
-    v_baseCuts.push_back("weighttaudecay");
+    //v_baseCuts.push_back("weighttaudecay");
 
     vector<TString> v_Cuts = v_baseCuts;
 
-
+    //need to put 8 TeV cms2 ttbar ntuples here
     if (true)
     {
         TChain  *ch_ttbar = new TChain("Events");
@@ -65,7 +39,6 @@ void doAll_nocuts(TString outputDir = "results")
         baby->ScanChain(ch_ttbar, v_Cuts, "ttdil", lumiToNormalizeTo * (154. / 157.5) * (190.41256 / 147.4)); //must mutliply by ratio of per-event xsec and PREP xsec to account for negative weights
 
         hist::color("ttdil", kGreen);
-        //delete ch_ttbar;
     }
     if (false)
     {
@@ -84,8 +57,8 @@ void doAll_nocuts(TString outputDir = "results")
         cout << "Doing the scale down mc@nlo ttbar sys sample" << endl; ch_ttbar->Add("/nfs-7/userdata/cms2/FastSim_mcatnlo-private-SC-fac-0.5-ren-0.5_LHE2EDM_v1_RWTH_0614_fhohle-TT_mcatnlo_private_fac-0.5-ren-0.5_withSC_FastSim_coherent_Summer11FullSim_FastSimJetFixTest_AOD_0616-98893edf89c918d9b6d63453d739e0c0/VB04-02-29_Fastsim/merged*root");
         //cout << "Doing the scale up mc@nlo ttbar sys sample" << endl; ch_ttbar->Add("/nfs-7/userdata/cms2/FastSim_mcatnlo-private-SC-fac-2-ren-2_LHE2EDM_v1_RWTH_0614_fhohle-TT_mcatnlo_private_fac-2-ren-2_withSC_FastSim_coherent_Summer11FullSim_FastSimJetFixTest_AOD_0616-98893edf89c918d9b6d63453d739e0c0/VB04-02-29_Fastsim/merged*root");
         baby->ScanChain(ch_ttbar, v_Cuts, "ttdil", lumiToNormalizeTo * 154. / 157.5);
-        hist::color("ttdil", kGreen);
 
+        hist::color("ttdil", kGreen);
     }
 
 
@@ -98,8 +71,6 @@ void doAll_nocuts(TString outputDir = "results")
     cout << "done saving" << endl;
 
     hist::deleteHistos();
-
-
 
 }
 
