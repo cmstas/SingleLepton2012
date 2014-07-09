c56b8febf350eca6fda65ac456b6a4b018c70269
linacre <linacre@fnal.gov>
2014-07-09 12:30:56 -0700

    update for 8 TeV, tweak binning so edges align with input histos

--- macros/TopAFBLooper/denominator/acceptanceplots.C
+++ macros/TopAFBLooper/denominator/acceptanceplots.C
@@ -10,9 +10,10 @@
 #include "TPaveText.h"
 #include "THStack.h"
 #include "TLatex.h"
+#include "TMath.h"
 #include <fstream>
-#include "tdrStyle.C"
-#include "CommonFunctions.C"
+#include "../tdrStyle.C"
+#include "../CommonFunctions.C"
 #include <vector>
 
 using namespace std;
@@ -69,16 +70,32 @@ void GetAfb(TH1D* h, Double_t &afb, Double_t  &afberr){
 
 
 
-void acceptanceplots(TString histname = "lepAzimAsym", bool drawnorm = false, TString FName1 = "results/hist_usePtGt2020_hypDisamb_usepfMET_usepfJets_useOS_vetoHypMassLt12_requireBTag_sortJetCandidatesbyPt_generalLeptonVeto_createBabyNtuples_applylepIDCuts_applylepIsoCuts_vetoZmass_veto2Jets_vetoMET.root", TString FName2 = "results/hist_noCuts.root"){
+void acceptanceplots(TString histname = "lepAzimAsym2", bool drawnorm = false, TString FName1 = "../SIGoutput/ttdl_mcatnlo_smallTree_histos.root", TString FName2 = "results/hist_noCuts.root"){
+
+  TString observablename = "";
+
+  if(histname=="lepChargeAsym") observablename="lep_charge_asymmetry";
+  if(histname=="lepAzimAsym2") observablename="lep_azimuthal_asymmetry2";
+  if(histname=="lepAzimAsym") observablename="lep_azimuthal_asymmetry";
+  if(histname=="lepPlusCosTheta") observablename="lepPlus_costheta_cms";
+  if(histname=="lepMinusCosTheta") observablename="lepMinus_costheta_cms";
+  if(histname=="lepCosTheta") observablename="lep_costheta_cms";
+  if(histname=="topSpinCorr") observablename="top_spin_correlation";
+  if(histname=="rapiditydiffMarco") observablename="top_rapiditydiff_Marco";
+  if(histname=="topCosTheta") observablename="top_costheta_cms";
+  if(histname=="lepCosOpeningAngle") observablename="lep_cos_opening_angle";
+  if(histname=="pseudorapiditydiff") observablename="top_pseudorapiditydiff_cms";
+  if(histname=="rapiditydiff") observablename="top_rapiditydiff_cms";
+
   setTDRStyle();
 
 
   std::cout << "Opening " << FName1.Data() << "\n";
   TFile *f_1         = TFile::Open(FName1.Data());  
-  hnumerator = (TH1D*)f_1->Get(Form("ttdil_h%sGen_allj_all", histname.Data())); 
-  hnumerator2d_mtt = (TH2D*)f_1->Get(Form("ttdil_h%sGen2d_allj_all", histname.Data())); 
-  hnumerator2d_ttpt = (TH2D*)f_1->Get(Form("ttdil_h%sttpTGen2d_allj_all", histname.Data())); 
-  hnumerator2d_ttrapidity2 = (TH2D*)f_1->Get(Form("ttdil_h%sttRapidity2Gen2d_allj_all", histname.Data())); 
+  hnumerator = (TH1D*)f_1->Get(Form("h_numerator_%s_gen_all", observablename.Data())); 
+  hnumerator2d_mtt = (TH2D*)f_1->Get(Form("h_numerator_%s_vs_mtt_gen_all", observablename.Data())); 
+  hnumerator2d_ttpt = (TH2D*)f_1->Get(Form("h_numerator_%s_vs_ttpt_gen_all", observablename.Data())); 
+  hnumerator2d_ttrapidity2 = (TH2D*)f_1->Get(Form("h_numerator_%s_vs_ttrapidity2_gen_all", observablename.Data())); 
 
   std::cout << "Opening " << FName2.Data() << "\n";  
   TFile *f_2         = TFile::Open(FName2.Data());  
@@ -88,27 +105,33 @@ void acceptanceplots(TString histname = "lepAzimAsym", bool drawnorm = false, TS
   hdenominator2d_ttrapidity2 = (TH2D*)f_2->Get(Form("ttdil_h%sttRapidity2Gen2d_allj_all", histname.Data())); 
 
 
-  std::cout << "Opened " << Form("ttdil_h%sGen_allj_all", histname.Data()) << " and "<< Form("ttdil_h%sGen2d_allj_all", histname.Data()) <<"\n";
+  std::cout << "Opened " << Form("h_numerator_%s_gen", observablename.Data()) << " and "<< Form("ttdil_h%sGen_allj_all", histname.Data()) <<"\n";
 
-  Double_t pi = 3.141592653589793;
+  Double_t pi = TMath::Pi();
   // These get copied into the array called "bins"
   Double_t bins_lepChargeAsym[] =  { -2., -1.4, -0.8, -0.6, -0.4, -0.2, 0., 0.2, 0.4, 0.6, 0.8, 1.4, 2.};
   //Double_t bins_lepChargeAsym[] =  { -2., -0.8, -0.4, 0., 0.4, 0.8, 2.};
-  Double_t bins_lepAzimAsym2[] = {0., 2.*pi/20., 4.*pi/20., 5.5*pi/20., 7.*pi/20., 8.5*pi/20., 10.*pi/20., 11.5*pi/20., 13.*pi/20., 14.5*pi/20., 16.*pi/20., 18*pi/20., pi}; 
+  Double_t bins_lepAzimAsym2[] = {0., 5.*pi/60., 10.*pi/60., 15.*pi/60., 20.*pi/60., 25.*pi/60., 30.*pi/60., 35.*pi/60., 40.*pi/60., 45.*pi/60., 50.*pi/60., 55.*pi/60., pi};
+  //Double_t bins_lepAzimAsym2[] = {0., 2.*pi/20., 4.*pi/20., 5.5*pi/20., 7.*pi/20., 8.5*pi/20., 10.*pi/20., 11.5*pi/20., 13.*pi/20., 14.5*pi/20., 16.*pi/20., 18*pi/20., pi}; 
   //Double_t bins_lepAzimAsym2[] = {0., 4.*pi/20., 7.*pi/20., 10.*pi/20., 13.*pi/20., 16.*pi/20., pi};
-  Double_t bins_lepAzimAsym[] = {-1., -0.8, -0.4, 0., 0.4, 0.8, 1.}; 
+  Double_t bins_lepAzimAsym[] = {-pi, -50.*pi/60., -40.*pi/60., -30.*pi/60., -20.*pi/60., -10.*pi/60.,  0., 10.*pi/60., 20.*pi/60., 30.*pi/60., 40.*pi/60., 50.*pi/60., pi};
+  //Double_t bins_lepAzimAsym[] = {-pi, -16.*pi/20., -13.*pi/20., -10.*pi/20., -7.*pi/20., -4.*pi/20.,  0., 4.*pi/20., 7.*pi/20., 10.*pi/20., 13.*pi/20., 16.*pi/20., pi};
+  //Double_t bins_lepAzimAsym[] = {-1., -0.8, -0.4, 0., 0.4, 0.8, 1.};
   Double_t bins_topCosTheta[] = {-1., -0.7, -0.4, 0., 0.4, 0.7, 1.}; 
-  Double_t bins_pseudorapiditydiff[] =  { -2., -1.0, -0.5, 0., 0.5, 1.0, 2.}; 
-  Double_t bins_rapiditydiff[] =  { -2., -0.8, -0.3, 0., 0.3, 0.8, 2.}; 
-  Double_t bins_rapiditydiffMarco[] =  { -2., -0.7, -0.3, 0., 0.3, 0.7, 2.}; 
+  Double_t bins_pseudorapiditydiff[] =  { -2., -1.0, -28./60., 0., 28./60., 1.0, 2.}; 
+  //Double_t bins_pseudorapiditydiff[] =  { -2., -1.0, -0.5, 0., 0.5, 1.0, 2.}; 
+  Double_t bins_rapiditydiff[] =  { -2., -1.0, -20./60., 0., 20./60., 1.0, 2.}; 
+  //Double_t bins_rapiditydiff[] =  { -2., -0.8, -0.3, 0., 0.3, 0.8, 2.}; 
+  Double_t bins_rapiditydiffMarco[] =  { -2., -44./60., -20./60., 0., 20./60., 44./60., 2.}; 
+  //Double_t bins_rapiditydiffMarco[] =  { -2., -0.7, -0.3, 0., 0.3, 0.7, 2.}; 
   Double_t bins_lepCosTheta[] = {-1., -0.6, -0.3, 0., 0.3, 0.6, 1.}; 
   Double_t bins_topSpinCorr[] = {-1., -0.5, -0.2, 0., 0.2, 0.5, 1.}; 
   Double_t bins_lepCosOpeningAngle[] = {-1., -0.6, -0.3, 0., 0.3, 0.6, 1.}; 
 
   // These get copied into the array called "binsfor2D"
   Double_t bins_lepChargeAsym_for2D[] =  { -2., 0., 2.};
-  Double_t bins_lepAzimAsym_for2D[] = {-1., 0., 1.};
   Double_t bins_lepAzimAsym2_for2D[] = {0., pi/2., pi};
+  Double_t bins_lepAzimAsym_for2D[] = {-pi, 0., pi};
   Double_t bins_topCosTheta_for2D[] = {-1., 0., 1.};
   Double_t bins_pseudorapiditydiff_for2D[] =  { -2., 0., 2.};
   Double_t bins_rapiditydiff_for2D[] =  { -2., 0., 2.};
@@ -121,17 +144,20 @@ void acceptanceplots(TString histname = "lepAzimAsym", bool drawnorm = false, TS
   //Double_t binsmtt[] = {0., 410., 510., 800.}; 
   Double_t binsttpt[] = {0., 41., 92., 300.}; 
   Double_t binsttrapidity2[] = {0., 0.34, 0.75, 1.5}; 
-  Double_t bins[13];
   Double_t binsfor2D[3];
 
   int nbinsx = -999;
+  if(histname == "lepChargeAsym" || histname == "lepAzimAsym2" || histname == "lepAzimAsym") nbinsx = 12;
+  else nbinsx = 6;
 
+  Double_t bins[nbinsx+1];
 
   if(histname == "lepChargeAsym") memcpy(bins,bins_lepChargeAsym,13*8);
   //if(histname == "lepChargeAsym") memcpy(bins,bins_lepChargeAsym,7*8);
   if(histname == "lepAzimAsym2") memcpy(bins,bins_lepAzimAsym2,13*8);
   //if(histname == "lepAzimAsym2") memcpy(bins,bins_lepAzimAsym2,7*8);
-  if(histname == "lepAzimAsym") memcpy(bins,bins_lepAzimAsym,7*8);
+  if(histname == "lepAzimAsym") memcpy(bins,bins_lepAzimAsym,13*8);
+  //if(histname == "lepAzimAsym") memcpy(bins,bins_lepAzimAsym,7*8);
   if(histname == "topCosTheta") memcpy(bins,bins_topCosTheta,7*8);
   if(histname == "pseudorapiditydiff") memcpy(bins,bins_pseudorapiditydiff,7*8);
   if(histname == "rapiditydiff") memcpy(bins,bins_rapiditydiff,7*8);
@@ -151,8 +177,15 @@ void acceptanceplots(TString histname = "lepAzimAsym", bool drawnorm = false, TS
   if(histname == "topSpinCorr") memcpy(binsfor2D,bins_topSpinCorr_for2D,3*8);
   if(histname == "lepCosOpeningAngle") memcpy(binsfor2D,bins_lepCosOpeningAngle_for2D,3*8);
 
-  if(histname == "lepChargeAsym" || histname == "lepAzimAsym2") nbinsx = 12;
-  else nbinsx = 6;
+
+  cout<<"Numerator has "<<hnumerator->GetNbinsX()<<" bins from "<<hnumerator->GetXaxis()->GetXmin()<<" to "<<hnumerator->GetXaxis()->GetXmax()<<endl;
+  cout<<"Denominator has "<<hdenominator->GetNbinsX()<<" bins from "<<hdenominator->GetXaxis()->GetXmin()<<" to "<<hdenominator->GetXaxis()->GetXmax()<<endl;
+  cout<<"Using "<<nbinsx<<" bins from "<<bins[0]<<" to "<<bins[nbinsx]<<endl;
+
+  if(hnumerator->GetNbinsX()!=hdenominator->GetNbinsX()) cout<<"***numerator and denominator do not match***"<<endl;
+  if(hnumerator->GetXaxis()->GetXmax()!=hdenominator->GetXaxis()->GetXmax()) cout<<"***numerator and denominator do not match***"<<endl;
+  if(hnumerator->GetXaxis()->GetXmin()!=hdenominator->GetXaxis()->GetXmin()) cout<<"***numerator and denominator do not match***"<<endl;
+
 
   hnumerator = (TH1D*) hnumerator->Rebin(nbinsx,Form("numerator_%s", histname.Data()),bins);
   hdenominator = (TH1D*) hdenominator->Rebin(nbinsx,Form("denominator_%s", histname.Data()),bins);
@@ -293,9 +326,9 @@ void acceptanceplots(TString histname = "lepAzimAsym", bool drawnorm = false, TS
   double Asym1,Asym2,Asym3;
   double Asym1err,Asym2err,Asym3err;
   
-  hacceptance_copy = new TH1D("hac","hac",6, bins);
-  hacceptance_statup = new TH1D("hacup","hacup",6, bins);
-  hacceptance_statdown = new TH1D("hacdown","hacdown",6, bins);
+  hacceptance_copy = new TH1D("hac","hac",nbinsx, bins);
+  hacceptance_statup = new TH1D("hacup","hacup",nbinsx, bins);
+  hacceptance_statdown = new TH1D("hacdown","hacdown",nbinsx, bins);
   TAxis *xaxis_ = hacceptance->GetXaxis();
   for (int i=1;i<=xaxis_->GetNbins();i++) {
     hacceptance_copy->Fill(xaxis_->GetBinCenter(i), hacceptance->GetBinContent(i));
@@ -364,7 +397,7 @@ void acceptanceplots(TString histname = "lepAzimAsym", bool drawnorm = false, TS
 
     max*=1.2;
 
-    TH1 *frame=new TH1F("frame","",1000,bins[0],bins[6]);frame->SetMaximum(max);frame->SetMinimum(min);frame->Draw();
+    TH1 *frame=new TH1F("frame","",1000,bins[0],bins[nbinsx]);frame->SetMaximum(max);frame->SetMinimum(min);frame->Draw();
     frame->GetXaxis()->SetTitle( hacceptance->GetXaxis()->GetTitle() );
     frame->GetYaxis()->SetTitle( "Normalized to unit area");
 
@@ -378,6 +411,7 @@ void acceptanceplots(TString histname = "lepAzimAsym", bool drawnorm = false, TS
   }
 
   TLegend *leg = new TLegend(0.48,0.78,0.90,0.92);
+  if(drawnorm) leg = new TLegend(0.48,0.78,0.90,0.88);
   leg->SetFillStyle(0);
   leg->SetLineColor(0);
   leg->SetTextSize(0.036);
@@ -388,17 +422,14 @@ void acceptanceplots(TString histname = "lepAzimAsym", bool drawnorm = false, TS
   if(drawnorm) leg->AddEntry(hdenominator, "denominator","l");
   leg->Draw("same");
 
-  //TPaveText *pt1 = new TPaveText(0.18, 0.86, 0.40, 0.92, "brNDC");
-  //if(drawnorm) pt1 = new TPaveText(0.18, 0.77, 0.40, 0.92, "brNDC");
   TPaveText *pt1 = new TPaveText(0.155, 0.94, 0.41, 0.98, "brNDC");
+  if(drawnorm) pt1 = new TPaveText(0.18, 0.77, 0.40, 0.92, "brNDC");
   pt1->SetName("pt1name");
   pt1->SetBorderSize(0);
   pt1->SetFillStyle(0);
 
   TText *blah;
-  //blah = pt1->AddText("CMS Preliminary, 5.0 fb^{-1} at  #sqrt{s}=7 TeV");
-  //blah = pt1->AddText("CMS, 5.0 fb^{-1} at  #sqrt{s}=7 TeV");
-  blah = pt1->AddText("CMS Simulation, #sqrt{s} = 7 TeV");
+  blah = pt1->AddText("CMS Simulation, #sqrt{s} = 8 TeV");
   blah->SetTextSize(0.036);
   blah->SetTextAlign(11);   
 
